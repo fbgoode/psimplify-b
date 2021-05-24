7ce5e64d26083fc1d3fc08d62383d20ff956e2ab
Fede <fedebaez@gmail.com>
2021-05-24 19:59:17 +0200

    Fixed reset password issue with cognito PostConfirmation.

--- debug.js
+++ debug.js
@@ -2,17 +2,17 @@ require('dotenv').config();
 
 const connectToDatabase = require('./src/framework/mongodb');
 
-// const { handler: registerfn } = require('./src/framework/aws/cognito/register');
-// const cognitoevent = require('./events/register.json');
+const { handler: registerfn } = require('./src/framework/aws/cognito/register');
+const cognitoevent = require('./events/register.json');
 
-const adapters = require('./src/framework/express/unprotected/adapters');
-const event = require('./events/getSlots.json');
+// const adapters = require('./src/framework/express/unprotected/adapters');
+// const event = require('./events/getSlots.json');
 
 (async () => {
     await connectToDatabase(null,null,()=>{});
-    // const result = await registerfn(cognitoevent);
+    const result = await registerfn(cognitoevent);
     
-    const result = await adapters.getSlots(event.req,event.res);
+    // const result = await adapters.getSlots(event.req,event.res);
 
     console.log(result);
 
--- src/useCases/customer/user/registerUser.js
+++ src/useCases/customer/user/registerUser.js
@@ -1,5 +1,7 @@
 module.exports = function makeRegisterUser ({ userdb }) {
   return async function registerUser (userInfo) {
+    const existingUser = await userdb.getById(userInfo._id);
+    if(existingUser) return existingUser;
     const newUser = await userdb.add(userInfo);
     return newUser;
   }
